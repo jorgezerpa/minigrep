615e86ffd0ff459282d592e069b03982acc8f986
Jorge Zerpa <jorgezerpacoder@gmail.com>
2024-09-28 10:47:05 -0400

    Refactoring and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
 use std::env; // for take command line arguments
 use std::fs;
+use std::process; 
 
 fn main() {
     let args:Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
+    println!("----",);
 
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
-    println!("----",);
+    run(config);
+
     
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading file.");
-    
+}
+
+fn run(config:Config) {
+    let contents = fs::read_to_string(config.filename).unwrap_or_else(|err| {
+        println!("Error reading file: {}",err);
+        process::exit(1);
+    });
     println!("{}", contents);
-    println!("----",);
+}
+
+struct Config {
+    query:String,
+    filename:String,
+}
+
+impl Config {
+    fn new(args:&[String]) -> Result<Config, &str> {
+        if args.len()<3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { filename, query })
+    }
 }
