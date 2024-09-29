566d69496c2c8e21604aee0241e167226e3746de
Jorge Zerpa <jorgezerpacoder@gmail.com>
2024-09-29 09:47:56 -0400

    MVP

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ pub fn run(config:Config) {
         println!("Error reading file: {}",err);
         process::exit(1);
     });
-    println!("{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line); 
+    }
 }
 
 pub struct Config {
@@ -24,4 +26,32 @@ impl Config {
     
         Ok(Config { filename, query })
     }
+}
+
+pub fn search<'a>(query:&str, contents:&'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+            assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
 }
